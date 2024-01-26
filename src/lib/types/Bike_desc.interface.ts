d17e631fd8af77b628b363b03fcb3a6a9cc32f7f
Hand2MartIndia <134159036+Hand2MartIndia@users.noreply.github.com>
2024-01-26 21:18:43 +0530

    insurance upto will be string type

--- src/lib/types/Bike_desc.interface.ts
+++ src/lib/types/Bike_desc.interface.ts
@@ -2,7 +2,7 @@ interface bike_description {
     kms_driven: number;
     mileage: number;
     insurance_status:number;
-    insurance_upto:number;
+    insurance_upto:string;
     fuel_type: string;
     last_price:number;
 }
